183cf44fedeedbf5edee0aee001854d7d90da512
Jiaran Yu <57048254+jiaranyu@users.noreply.github.com>
2020-10-16 15:48:52 -0700

    Update kaggle-jiarany-1.py

--- kaggle-jiarany-1.py
+++ kaggle-jiarany-1.py
@@ -31,10 +31,6 @@ from sklearn.gaussian_process.kernels import DotProduct, WhiteKernel, RBF
 # In[45]:
 from sklearn.linear_model import BayesianRidge, LinearRegression
 
-def load_df():
-    train = pd.read_csv('train2.txt',  sep=',')
-    test = pd.read_csv('test2.txt', sep=',')
-    return train, test
 
 def cal_time(x):
     return (x - pd.Timestamp('2020-01-01'))
@@ -65,95 +61,7 @@ def convert_label(df, headers):
         h = headers[i]
         df[h] = df[h].apply(label_helper(df[h]).get)
 
-def get_sympts_list(df):
-    raw_symptoms_list = df['symptoms'].value_counts().keys()
-    dic = set()
-    for smps in raw_symptoms_list:
-        for w in smps.split('; '):
-            dic.add(w.lower())
-    return dic
-        
-def get_filtered_sympts(dic):
-    filtered_dict = set()
-    for i in dic:
-        if i in syptsMap.keys():
-            filtered_dict.add(syptsMap[i])
-        else:
-            filtered_dict.add(i)
-    return filtered_dict
-
     
-syptsMap = {'fever (38-39 ° c)': 'high fever', 
-            'fever (38-39 ℃)': 'high fever',
-            'fever (39.5 ℃)': 'high fever',
-            'fever 38.3': 'high fever',
-            'low fever (37.2 ° c)': 'fever',
-            'toothache':'fever',
-            'fever (37 ℃)': 'fever',
-           'low fever 37.0 ℃':'fever',
-            'low fever (37.4 ℃)':'fever',
-           'fever 37.7℃':'fever',
-            'diarrhoea':'diarrhea',
-            'abdominal pain':'diarrhea',
-            'diarrheoa':'diarrhea',
-            'shortness breath': 'breath',
-            'shortness of breath':'breath',
-            'anhelation':'breath',
-            'difficulty breathing':'breath',
-            'systemic weakness':'weak',
-            'poor physical condition':'weak',
-            'anorexia':'weak',
-            'feeling ill':'weak',
-            'physical discomfort':'weak',
-            'malaise':'weak',
-            'general malaise':'weak',
-            'discomfort':'weak',
-            'fatigue':'weak',
-            'weakness':'weak',
-            'pharyngeal dryness':'throat',
-            'pharyngeal discomfort':'throat',
-             'pharynx':'throat',
-            'sore throat':'throat',
-            'dry throat':'throat',
-            'throat discomfort':'throat',
-            'myalgia':'muscle',
-            'muscle ache': 'muscle',
-            'muscular soreness': 'muscle',
-            'muscular stiffness': 'muscle',
-            'sore body':'muscle',
-            'joint pain':'muscle',
-            'soreness':'muscle',
-            'chest distress':'chest',
-            'pleuritic chest pain':'chest',
-            'chest tightness':'chest',
-            'chest pain':'chest',
-            'pneumonitis':'pneumonia',
-            'dry cough':'cough',
-            'other symptoms':'cough',
-            'coughing':'cough',
-            'nausea':'vomiting',
-             'expectoration':'sputum',
-            'sneezing':'sneeze',
-            
-            'rhinorrhoea':'rhinorrhea',
-            'nasal congestion':'nose',
-            'runny nose':'nose',
-            'dyspnea': 'severe pneumonia',
-           }
-
-def label_transform(col):
-    le = preprocessing.LabelEncoder()
-    le.fit(list(train[col].value_counts().keys()) + list(test[col].value_counts().keys()))
-    train[col] = le.transform(train[col])
-    test[col] = le.transform(test[col])
-    
-    
-
-
-
-# In[451]:
-
-
 train = pd.read_csv('train_clean1.csv')
 test = pd.read_csv('test_clean1.csv')
 
@@ -169,63 +77,6 @@ label_transform('country')
 label_transform('V1')
 train['confirmed'] = train['confirmed'].apply(cal_average)
 test['confirmed'] = test['confirmed'].apply(cal_average)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[583]:
 
 
 X_train, X_test, y_train, y_test = train_test_split(train.loc[:, (train.columns != 'duration') 
@@ -245,54 +96,10 @@ regr = svm.SVR(kernel='linear').fit(X_train, y_train)
 predict_result = regr.predict(X_test)
 mse = (mean_squared_error(list(predict_result_gussian[0]), y_test))**(1/2)
 
-
-# In[581]:
-
-
-mse
-
-
-# In[578]:
-
-
-# predict_result = regr.predict(test_train)
-
-# result_df = pd.DataFrame(columns=['Id', 'duration'])
-# result_df['Id'] = test['Id']+1
-# result_df['duration'] = predict_result
-# result_df.to_csv('result_svm.csv',index=False)
-
-
-# In[579]:
-
-
 predict_result_gussian = gpr.predict(test_train, return_std=True)
 result_df = pd.DataFrame(columns=['Id', 'duration'])
 result_df['Id'] = test['Id']+1
 result_df['duration'] = predict_result_gussian[0]
 result_df.to_csv('result_guassian.csv',index=False)
-
-
-# In[587]:
-
-
-
-
-
-# In[588]:
-
-
-
-
-
-# In[589]:
-
-
-
-
-
-# In[ ]:
-
-
 
 
